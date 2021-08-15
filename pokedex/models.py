b2ed8e38b97dca2aafbc14273627ea18f4146406
adityakadoo <aditya1449kadoo@gmail.com>
2021-08-15 16:08:02 +0530

    added Versions and Abilities

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models.aggregates import Max
+from django.db.models.base import Model
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+class Version(models.Model):
+    """Models a pokemon game version"""
+    index = models.IntegerField(null=True)
+    name = models.CharField(max_length=50,null=True)
+    generation = models.IntegerField(null=True)
+
+class Ability(models.Model):
+    """Models a pokemon ability"""
+    index = models.IntegerField(null=True)
+    name = models.CharField(max_length=50,null=True)
+    effect = models.CharField(max_length=2000,null=True)
+    versions = models.ManyToManyField(Version, through='Ability_var',null=True)
+
+class Ability_var(models.Model):
+    """Models the variations of a pokemon ablility between different versions"""
+    ability = models.ForeignKey(Ability, on_delete=models.CASCADE,null=True)
+    version = models.ForeignKey(Version, on_delete=models.CASCADE,null=True)
+    description = models.CharField(max_length=500,null=True)
+    is_present = models.BooleanField(null=True)
